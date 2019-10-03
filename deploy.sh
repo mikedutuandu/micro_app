e64f860a17c1bf50a58286b7b0b10cca70baad65
mikedu <mikedutuandu@gmail.com>
2019-10-03 11:45:47 +0700

    update deploy file

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,24 @@
-docker build -t mikedutuandu/multi-client:latest -t mikedutuandu/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t mikedutuandu/multi-server:latest -t mikedutuandu/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t mikedutuandu/multi-worker:latest -t mikedutuandu/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker build -t mikedutuandu/micro_teacher:latest -t mikedutuandu/micro_teacher:$SHA -f ./micro_teacher/Dockerfile ./micro_teacher
+docker build -t mikedutuandu/micro_learner:latest -t mikedutuandu/micro_learner:$SHA -f ./micro_learner/Dockerfile ./micro_learner
+docker build -t mikedutuandu/micro_booking:latest -t mikedutuandu/micro_booking:$SHA -f ./micro_booking/Dockerfile ./micro_booking
+docker build -t mikedutuandu/gateway_teacher:latest -t mikedutuandu/gateway_teacher:$SHA -f ./gateway_teacher/Dockerfile ./gateway_teacher
+docker build -t mikedutuandu/gateway_learner:latest -t mikedutuandu/gateway_learner:$SHA -f ./gateway_learner/Dockerfile ./gateway_learner
 
-docker push mikedutuandu/multi-client:latest
-docker push mikedutuandu/multi-server:latest
-docker push mikedutuandu/multi-worker:latest
+docker push mikedutuandu/micro_teacher:latest
+docker push mikedutuandu/micro_learner:latest
+docker push mikedutuandu/micro_booking:latest
+docker push mikedutuandu/gateway_teacher:latest
+docker push mikedutuandu/gateway_learner:latest
 
-docker push mikedutuandu/multi-client:$SHA
-docker push mikedutuandu/multi-server:$SHA
-docker push mikedutuandu/multi-worker:$SHA
+docker push mikedutuandu/micro_teacher:$SHA
+docker push mikedutuandu/micro_learner:$SHA
+docker push mikedutuandu/micro_booking:$SHA
+docker push mikedutuandu/gateway_teacher:$SHA
+docker push mikedutuandu/gateway_learner:$SHA
 
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=mikedutuandu/multi-server:$SHA
-kubectl set image deployments/client-deployment client=mikedutuandu/multi-client:$SHA
-kubectl set image deployments/worker-deployment worker=mikedutuandu/multi-worker:$SHA
+kubectl set image deployments/micro_teacher_deployment server=mikedutuandu/micro_teacher:$SHA
+kubectl set image deployments/micro_learner_deployment server=mikedutuandu/micro_learner:$SHA
+kubectl set image deployments/micro_booking_deployment server=mikedutuandu/micro_booking:$SHA
+kubectl set image deployments/gateway_teacher_deployment server=mikedutuandu/gateway_teacher:$SHA
+kubectl set image deployments/gateway_learner_deployment server=mikedutuandu/gateway_learner:$SHA
